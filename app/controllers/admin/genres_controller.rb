48c83987bdcf93504ff015771142b71a1c0d846b
A24-N <117143210+A24-N@users.noreply.github.com>
2022-12-17 03:12:55 +0000

    [add]index_form

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,16 @@
 class Admin::GenresController < ApplicationController
+
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
