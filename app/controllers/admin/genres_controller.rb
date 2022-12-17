dc88412c91ffdbf8ac277c52529ce3e1488b8de0
A24-N <117143210+A24-N@users.noreply.github.com>
2022-12-17 05:32:29 +0000

    [Fnish]Add_admin/genres

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -12,13 +12,17 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    @genre = Genre.find(params[:id])
+    @genre.update(genre_params)
+    redirect_to admin_genres_path, notice: "ジャンルを編集しました。"
   end
-  
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
