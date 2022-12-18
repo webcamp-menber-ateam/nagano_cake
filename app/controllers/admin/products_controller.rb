76eb5c304c69a1127f37f36896c1cb8e617332ec
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2022-12-18 07:38:01 +0000

    [Update]admin/products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,7 +5,7 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
-    @genres = Genre.all
+    @product = Product.find(params[:id])
   end
 
   def new
@@ -16,16 +16,23 @@ class Admin::ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to admin_product_path(@product)
+      redirect_to admin_product_path(@product), notice: "商品を登録しました。"
     else
       render :new
     end
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admin_product_path(@product), notice: "商品を編集しました。"
+    else
+      render :edit
+    end
   end
 
   private
