d33bc5adce0f243a5cc56fd9dfb8baba063f4b4f
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2022-12-18 09:14:40 +0000

    [Start]public/addresses

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,24 @@
 class Public::AddressesController < ApplicationController
+
+  def index
+
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postcode, :name, :address)
+  end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :customer
+  
+  
 end
