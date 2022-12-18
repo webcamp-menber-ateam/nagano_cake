2fb88872bdbe18c7e1fe7e0c8a43c5c6a460dcbf
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2022-12-18 05:05:14 +0000

    [Update]admin/products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -14,12 +14,24 @@ class Admin::ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :genre_id, :name, :explanation, :price, :is_active)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,18 @@ class Product < ApplicationRecord
   has_many :carts, dependent: :destroy
   has_many :order_details, dependent: :destroy
 
-  
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+  enum is_active: { sale: true, stop_selling: false }
+  validates :is_active, presence: true, inclusion: { in: [true, false] }
+
+  has_one_attached :image
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image.jpg")
+      image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+    end
+    image
+  end
 end
