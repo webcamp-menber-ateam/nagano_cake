b029cdf47dfe41b89675ece43ef9f64d3f2b30ee
yuken02 <git@github.com:yuken02/practice.git>
2022-12-18 11:28:42 +0000

    [Add]ユーザー退会機能

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,6 +8,25 @@ class Public::CustomersController < ApplicationController
   end
 
   def update
-    @current_customer = current_customer
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to my_page_customers_path
+    else
+      render "edit"
+    end
+  end
+
+  def withdrawal
+    @customer = current_customer
+    @customer.update(is_customer_deleted: true)
+    reset_session
+    flash[:notice] = "退会が完了しました。"
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number, :email)
   end
 end
