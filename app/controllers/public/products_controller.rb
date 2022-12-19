c3e033ddb96ace88eb0e4dad62dfa2615f65a218
yuken02 <git@github.com:yuken02/practice.git>
2022-12-19 08:44:38 +0000

    [Add]public/products-index,show

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,11 @@
 class Public::ProductsController < ApplicationController
+  def index
+    @genres = Genre.all
+    @products = Product.page(params[:page])
+  end
+
+  def show
+    @genres = Genre.all
+    @product = Product.find(params[:id])
+  end
 end
