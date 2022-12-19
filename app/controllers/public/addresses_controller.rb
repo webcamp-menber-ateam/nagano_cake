b5eeaaacb55943cd95731add2c460a10165b91b2
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2022-12-19 01:39:39 +0000

    [Update]public/addresses/index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,9 @@
 class Public::AddressesController < ApplicationController
+  before_action :authenticate_customer!
 
   def index
-
+    @address = Address.new
+    @addresses = current_customer.addresses.all
   end
 
   def edit
