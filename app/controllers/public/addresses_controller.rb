20bd1389f2b485e6f93cac9599b90be4363dc429
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2022-12-19 05:03:21 +0000

    [Update]controllers/public/addresses_controller.rb

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -7,15 +7,31 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = current_customer.addresses.new(address_params)
+    if @address.save
+      redirect_to request.referer, notice: "配送先の情報を登録しました。"
+    else
+      @addresses = current_customer.addresses.all
+      render :index
+    end
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    Address.find(params[:id]).destroy
+    redirect_to request.referer, notice: "配送先の情報を削除しました。"
   end
 
   private
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
-  
-  validates :postcode, presence: true
+
+  validates :postcode, presence: true, length: { is: 7 }, numericality: {only_integer: true}
   validates :address, presence: true
   validates :name, presence: true
 end
