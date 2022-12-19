73b926506cd7c30689842c1431ced36410704e88
Yasufuku1 <115861222+yasufuku1@users.noreply.github.com>
2022-12-19 04:02:31 +0000

    [Update]models/address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :customer
   
-  validates :postcode
+  validates :postcode, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
 end
