d6d92b5d17d148b478a7395a05d9e4187de62151
A24-N <117143210+A24-N@users.noreply.github.com>
2022-12-19 01:39:53 +0000

    [Add]orders_controller_update

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,5 +5,19 @@ class Admin::OrdersController < ApplicationController
     @orderdetails = OrderDetail.where(order_id: @order.id)
     @total_price = 0
   end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetails.where(order_id @order.id)
+    if @order.update(order_params)
+      @order_details.update_all(creat_status: 1) if @order.order_status == "payment_confirmation"
+    end
+    redirect_to request.referer
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
 
 end
