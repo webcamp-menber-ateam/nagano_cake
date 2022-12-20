2d8872ec12e62ddd2a7db43803c07757836416b7
YsukeKuwano <115121908+Aukown1003@users.noreply.github.com>
2022-12-20 04:41:07 +0000

    [Fix]新規住所空欄時処理&comfilmリロード時処理

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,11 +3,11 @@ class Public::OrdersController < ApplicationController
     @customer = current_customer
     @order = Order.new
   end
-  
+
   def index
     @orders = current_customer.orders
   end
-  
+
   def confirm
     # ordersessionを作成
     session[:order] = Order.new(order_params)
@@ -19,7 +19,7 @@ class Public::OrdersController < ApplicationController
     carts = current_customer.carts #顧客のカートを呼び出し
     @total = carts.inject(0) { |sum, cart| sum + cart.subtoal }
     # 合計金額の保存(上記計算+送料)両方数値なのでto_i不要
-    session[:order][:total_price] = @total + session[:order][:postage]
+    session[:order][:total_price] = (@total + session[:order][:postage])
     # 注文ステータスの保存
     session[:order][:order_status] = 0
     # 支払い方法の保存
@@ -28,33 +28,36 @@ class Public::OrdersController < ApplicationController
     # 配送先処理
     delivery_destination = params[:order][:delivery_select].to_i
     if delivery_destination == 1
-      session[:order][:delivery_postcode] = current_customer.postcode
-      session[:order][:delivery_address] = current_customer.address
-      session[:order][:delivery_name] = (current_customer.last_name + current_customer.first_name)
+      session[:order][:delivery_postcode] = current_customer.postcode.freeze
+      session[:order][:delivery_address] = current_customer.address.freeze
+      session[:order][:delivery_name] = (current_customer.last_name + current_customer.first_name).freeze
     elsif delivery_destination == 2
       delivary = Address.find(id: params[:order][:delivery_adress])
-      session[:order][:delivery_postcode] = delivary.postcode
-      session[:order][:delivery_address] = delivary.address
-      session[:order][:delivery_name] = delivary.name
-    else #delivery_destination == 3
-      session[:order][:delivery_postcode] = params[:order][:delivery_postcode]
-      session[:order][:delivery_address] = params[:order][:delivery_address]
-      session[:order][:delivery_name] = params[:order][:delivery_name]
+      session[:order][:delivery_postcode] = delivary.postcode.freeze
+      session[:order][:delivery_address] = delivary.address.freeze
+      session[:order][:delivery_name] = delivary.name.freeze
+    else
+      session[:order][:delivery_postcode] = params[:order][:delivery_postcode].freeze
+      session[:order][:delivery_address] = params[:order][:delivery_address].freeze
+      session[:order][:delivery_name] = params[:order][:delivery_name].freeze
+    end
+    if delivery_destination == 3 && (params[:order][:delivery_postcode].blank? || params[:order][:delivery_address].blank? || params[:order][:delivery_name].blank?)
+      redirect_to request.referer, notice: "お届け先の入力不足項目があります"
     end
     @carts = current_customer.carts
     @order = session[:order]
   end
-  
+
   def complete
-    
+
   end
-  
+
   def create
     # トランザクション処理の指定(途中でエラーの場合は全てロールバックされる)
     ActiveRecord::Base.transaction do
       order = Order.new(session[:order])
       order.save
-      
+
       # 商品詳細の作成
       detail_item = current_customer.carts
       detail_item.each do |item|
@@ -73,16 +76,22 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+    # confirmでリロードされた時の対策
+    if params[:id] == "confirm"
+      redirect_to new_order_path, notice: "リロードされた為入力画面に戻りました"
+      return
+    end
+    
     @order = Order.find(params[:id])
     @order_details = @order.order_details
   end
-  
+
   private
   def order_params
     params.require(:order).permit(:customer_id, :postage, :total_price, :order_status,
     :payment_method, :delivery_postcode, :delivery_address, :delivery_name)
   end
-  
+
   def order_detail_params
     params.permit(:order_id, :product_id, :amount, :price, :creat_status)
   end
